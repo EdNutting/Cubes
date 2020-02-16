f9e5e47a2b24ffe1ef478202976b336f47737745
Ed Nutting <ed@beyondrisc.com>
2020-02-16 17:26:18 +0000

    Add position and solidity information to block data

--- Cubes/Assets/Scripts/Block.cs
+++ Cubes/Assets/Scripts/Block.cs
@@ -4,9 +4,12 @@ using UnityEngine;
 
 public interface IBlock
 {
-    
+    public Vector3Int pos { get; }
+    public bool isSolid { get;  }
 }
 
 public class Block : IBlock
 { 
+    public Vector3Int pos { get; set; }
+    public bool isSolid { get; set; }
 }
